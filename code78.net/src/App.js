650a929c811ba126a8ce9d909d21708466d96cee
C-McCarty <cmccarty5@collin.edu>
2025-06-05 17:52:40 -0500

    Created the coming soon page

--- code78.net/src/App.js
+++ code78.net/src/App.js
@@ -2,24 +2,28 @@ import logo from './logo.svg';
 import './App.css';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    return (
+        <div className="App">
+            <header>
+                <h1>code78.net</h1>
+                <nav>
+                    <a className='navBtn' href="https://www.youtube.com/watch?v=dQw4w9WgXcQ" target='_blank'>These</a>
+                    <a className='navBtn' href="https://www.youtube.com/watch?v=dQw4w9WgXcQ" target='_blank'>Links</a>
+                    <a className='navBtn' href="https://www.youtube.com/watch?v=dQw4w9WgXcQ" target='_blank'>Don't</a>
+                    <a className='navBtn' href="https://www.youtube.com/watch?v=dQw4w9WgXcQ" target='_blank'>Do</a>
+                    <a className='navBtn' href="https://www.youtube.com/watch?v=dQw4w9WgXcQ" target='_blank'>Anything</a>
+                    <a className='navBtn' href="https://www.youtube.com/watch?v=dQw4w9WgXcQ" target='_blank'>Useful</a>
+                    <a className='navBtn' href="https://www.youtube.com/watch?v=dQw4w9WgXcQ" target='_blank'>Yet</a>
+                </nav>
+            </header>
+            <div className="main">
+                <h2>Coming Soon to a code78.net near you!</h2>
+            </div>
+            <footer>
+                <small>Copyright © 2025 Code78.net - All Rights Reserved.</small>
+            </footer>
+        </div>
+    );
 }
 
 export default App;
